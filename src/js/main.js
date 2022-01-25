0c441ee37dc810545599330a462707b56d05086a
nasriime <mnasr4040@gmail.com>
2022-01-25 14:20:40 +0100

    list items

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,31 @@
 $('#hideBtn').click(function() {
   console.log('Hello world');
 });
-const list = document.createDocumentFragment();
-const ul = document.getElementById('offers');
+const wrapper = document.getElementById('offers');
+const url = 'https://mocki.io/v1/11356aa2-6371-41d4-9d49-77a5e9e9924f';
 
 var fetchData = () => {
-  fetch('https://mocki.io/v1/11356aa2-6371-41d4-9d49-77a5e9e9924f')
+  fetch(url)
   .then((data)=> {
     return data.json()
   })
   .then(results=>{
     results.map(function(result) {
-      let li = document.createElement('li');
-      let name = document.createElement('h2');
-      let operator_name = document.createElement('span');
+      const is_primary_image = result.images.filter(image=> image.is_primary === true);
+      const container = document.createElement('div');
+      container.class = 'list-item';
 
-      name.innerHTML = `${result.name}`;
-      operator_name.innerHTML = `${result.operator_name}`;
-
-      li.appendChild(name);
-      li.appendChild(operator_name);
-      list.appendChild(li);
+      let html = "";
+      html += '<div id="" class="">';
+      html += '<div class="">';
+      html += '<ul class="">';
+      html += `<img src="${is_primary_image.length > 0 ? is_primary_image[0]['url'] : ""}">`;
+      html += `<li class="">${result.name} <a href="javascript:void(0)"> <span class=""></span> </a> </li>`;
+      html += '</ul></div></div></div>';
+  
+      container.innerHTML = html;
+      wrapper.append(container);
     });
-    ul.appendChild(list);
   })
   .catch((error)=> {
     console.log(error);
