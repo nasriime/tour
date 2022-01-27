c37bf9e014ecba25aa6ee87ca1f1d6b1f9b31487
nasriime <mnasr4040@gmail.com>
2022-01-27 23:02:04 +0100

    list cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 $('#hideBtn').click(function() {
   console.log('Hello world');
 });
-const wrapper = document.getElementById('offers');
+const wrapper = document.getElementById('wrapper');
 const url = 'https://mocki.io/v1/11356aa2-6371-41d4-9d49-77a5e9e9924f';
 
 var fetchData = () => {
@@ -17,13 +17,72 @@ var fetchData = () => {
       const container = document.createElement('div');
       container.className = 'list-item';
 
-      let html = "";
-      html += '<div id="" class="">';
-      html += '<div class="">';
-      html += '<ul class="">';
-      html += `<img src="${is_primary_image.length > 0 ? is_primary_image[0]['url'] : ""}">`;
-      html += `<li class="">${name} <a href="javascript:void(0)"> <span class=""></span> </a> </li>`;
-      html += '</ul></div></div></div>';
+      let html = `
+        <li class="item">
+          <!-- left part -->
+          <div class="item__left">
+            <img src="${is_primary_image.length > 0 ? is_primary_image[0]['url'] : ""}" alt="">
+            <i class="far fa-heart item__left--like"></i>
+          </div>
+          <!-- middle part -->
+          <div class="item__middle">
+            <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, magnam.</h2>
+            <div class="item__middle--rating">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <span>2</span>
+              <span>reviews</span>
+            </div>
+            <p class="item__middle--review">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, quos!</p>
+            <div class="item__middle--info">
+              <div>
+                <span class="item__middle--info-label">Destinations</span>
+                <span>Rome</span>
+              </div>
+              <div>
+                <span class="item__middle--info-label">starts/ends in</span>
+                <span>london/london</span>
+              </div>
+              <div>
+                <span class="item__middle--info-label">operator</span>
+                <span>expat explore travel</span>
+              </div>
+            </div>
+          </div>
+          <!-- right part -->
+          <div class="item__right">
+            <div class="item__right--saving">
+              <div>-15%</div>
+            </div>
+            <div class="item__right--duration">
+              <div class="item__right--days">
+                <span>Duration</span>
+                <span>7 days</span>
+              </div>
+              <div class="item__right--price">
+                <span>From</span>
+                <span>2,134</span>
+              </div>
+            </div>
+            <div class="item__right--period">
+              <div class="item__right--availability-from">
+                <h6>1 Nov 2019</h6>
+                <h6>10+ sapces left</h6>
+              </div>
+              <div class="item__right--availability-to">
+                <h6>2 Nov 2019</h6>
+                <h6>10+ sapces left</h6>
+              </div>
+            </div>
+            <button class="item__right--btn">
+              <span>view tour</span><i class="fas fa-chevron-right"></i>
+            </button>
+          </div>
+        </li>
+      `;
   
       container.innerHTML = html;
       wrapper.append(container);
